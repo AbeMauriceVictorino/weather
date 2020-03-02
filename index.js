be712bcbde9f00faf09a268ae2d632c770ac6aa7
AbeMauriceVictorino <abe34@hawaii.edu>
2020-03-02 05:32:27 +0000

    I changed the weather api id to city

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ app.get("/", function(req, res) {
 //invoked after hitting go in the html form
 app.post("/", function(req, res) {
     
-    // takes in the zip from the html form, display in // console. Takes in as string, ex. for zip 02139
-        var zip = String(req.body.zipInput);;
-        console.log(req.body.zipInput);
+    // takes in the city from the html form, display in // console. Takes in as string.
+        var cityname = String(req.body.cityInput);
+        console.log(req.body.cityInput);
     
     //build up the URL for the JSON query, API Key is // secret and needs to be obtained by signup 
         const units = "imperial";
-        const apiKey = "67f6b382921c1e89b39b20d4f9556f22";
-        const url = "https://api.openweathermap.org/data/2.5/weather?zip=" + zip +  "&units=" + units + "&APPID=" + apiKey;
+        const apiKey = "cf0999ec010f30d149806a26e452653f";
+        const url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&units=" + units + "&APPID=" + apiKey;
     
     // this gets the data from Open WeatherPI
     https.get(url, function(response){
@@ -32,13 +32,19 @@ app.post("/", function(req, res) {
             const weatherData = JSON.parse(data);
             const temp = weatherData.main.temp;
             const city = weatherData.name;
-            const weatherDescription = weatherData.weather[0].description;
+            const humidity = weatherData.main.humidity;
+            const windSpeed = weatherData.wind.speed;
+            const weatherDescription = weatherData.weather[0].description; 
             const icon = weatherData.weather[0].icon;
-            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"; 
             
             // displays the output of the results
             res.write("<h1> The weather is " + weatherDescription + "<h1>");
-            res.write("<h2>The Temperature in " + city + " " + zip + " is " + temp + " Degrees Fahrenheit<h2>");
+            res.write("<h2> The Temperature in " + city + " is " + temp + "Degrees Fahrenheit" + "<h2>");
+            res.write("<h3>The Humidity is" + humidity + "<h3>");
+            res.write("<h4> The Wind Speed is " + windSpeed + "<h4>");
+            
+          
             res.write("<img src=" + imageURL +">");
             res.send();
         });
@@ -48,6 +54,6 @@ app.post("/", function(req, res) {
 
 //Commented out these lines in Repl
 //Uncomment these lines when running on laptop
-app.listen(3000, function() {
-console.log ("Server is running on port //3000")
+app.listen(process.env.PORT || 3001, function() {
+console.log ("Server is running on port")
 });
